c2105a7e8b422e32aa5553325a4c68fb46d08fa7
Rafael Silva <rafael.sdasilva84@hotmail.com>
2023-09-26 17:11:46 -0300

    create method telephonecontroller

--- FichaCadastroSln/FichaCadastroAPI/Controllers/TelephoneController.cs
+++ FichaCadastroSln/FichaCadastroAPI/Controllers/TelephoneController.cs
@@ -1,7 +1,9 @@
 ﻿using AutoMapper;
+using FichaCadastroAPI.DTO.Ficha;
 using FichaCadastroAPI.Model;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Net;
 
 namespace FichaCadastroAPI.Controllers
@@ -28,7 +30,30 @@ namespace FichaCadastroAPI.Controllers
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public ActionResult<TelephoneReadDTO> Post([FromBody] TelephoneCreateDTO telephoneCreateDTO)
         {
-            TelephoneModel telephoneModel = _mapper.Map<TelephoneModel>(telephoneCreateDTO);
+            try
+            {
+                _logger.LogCritical($"Dados {telephoneCreateDTO.ToString()}");
+                var ip = _httpContextAccessor
+                                    .HttpContext!
+                                    .Connection!
+                                    .RemoteIpAddress!
+                                    .ToString();
+
+                _logger.LogWarning($"IP Requisição {ip}");
+
+                bool existeFichaId = _fichaCadastroContextDB
+                                            .FichaModels
+                                            .ToList()
+                                            .Exists(exists => exists.Id == telephoneCreateDTO.FichaId);
+
+                if (existeFichaId==false)
+                {
+                    return StatusCode(HttpStatusCode.NotFound.GetHashCode(), "Não encontrada a ficha com id indicado");
+                }
+
+                TelephoneModel telephoneModel = _mapper.Map<TelephoneModel>(telephoneCreateDTO);
+
+
 
             _fichaCadastroContextDB.TelephoneModels.Add(telephoneModel);
             _fichaCadastroContextDB.SaveChanges();
@@ -36,6 +61,134 @@ namespace FichaCadastroAPI.Controllers
             TelephoneReadDTO telephoneReadDTO = _mapper.Map<TelephoneReadDTO>(telephoneModel);
 
             return StatusCode(HttpStatusCode.Created.GetHashCode(), telephoneReadDTO);
+
+            }
+            catch (Exception)
+            {
+
+                return BadRequest();
+            }
         }
+        
+        [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status409Conflict)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public ActionResult<TelephoneReadDTO> Put([FromRoute] int id, [FromBody] TelephoneUpdateDTO telephoneUpdateDTO)
+        {
+            try
+            {
+                TelephoneModel? telephoneModel = _fichaCadastroContextDB
+                                                                .TelephoneModels
+                                                                .Find(id);
+                //validação 
+                if (telephoneModel == null)
+                {
+                    return StatusCode(HttpStatusCode.NotFound.GetHashCode(), "Não pode alterar um registro que não exista na base de dados");
+                }
+
+                telephoneModel = _mapper.Map(telephoneUpdateDTO, telephoneModel);
+
+                _fichaCadastroContextDB.TelephoneModels.Update(telephoneModel);
+                _fichaCadastroContextDB.SaveChanges();
+
+                TelephoneReadDTO telephoneReadDTO = _mapper.Map<TelephoneReadDTO>(telephoneModel);
+
+                return StatusCode(HttpStatusCode.OK.GetHashCode(), telephoneReadDTO);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(HttpStatusCode.InternalServerError.GetHashCode(), ex);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status409Conflict)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]//  gera uma anotação no sweger informando os tipos de retorno do método
+        public ActionResult Delete([FromRoute] int id)
+        {
+            try
+            {
+                
+                TelephoneModel? telephoneModel = _fichaCadastroContextDB
+                                                    .TelephoneModels
+                                                    .Where(w => w.Id == id)
+                                                    .FirstOrDefault();
+
+                if (telephoneModel == null)
+                {
+                    return StatusCode(HttpStatusCode.NotFound.GetHashCode(), "Não existe dados a ser excluído");
+                }
+
+
+                _fichaCadastroContextDB.TelephoneModels.Remove(telephoneModel);
+                _fichaCadastroContextDB.SaveChanges();
+
+                return StatusCode(HttpStatusCode.OK.GetHashCode());
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(HttpStatusCode.InternalServerError.GetHashCode(), ex);
+            }
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public ActionResult<IEnumerable<FichaTelephoneReadDTO>> Get([FromQuery] int? FichaId)
+        {
+            try
+            {
+                List<TelephoneModel>? telephonesModel;
+
+                //validação
+               
+               
+                    telephonesModel = _fichaCadastroContextDB
+                                                    .TelephoneModels
+                                                    .ToList();
+                
+
+                IEnumerable<FichaTelephoneReadDTO> fichaTelephoneReadDTO = _mapper.Map<IEnumerable<FichaTelephoneReadDTO>>(telephonesModel);
+
+                return StatusCode(HttpStatusCode.OK.GetHashCode(), fichaTelephoneReadDTO);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(HttpStatusCode.InternalServerError.GetHashCode(), ex);
+            }
+        }
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public ActionResult<FichaTelephoneReadDTO> Get([FromRoute] int Fichaid)
+        {
+            try
+            {
+                
+                if (Fichaid == 0)
+                {
+                    return StatusCode(HttpStatusCode.NotFound.GetHashCode(), "Id está igual a zero");
+                }
+
+                TelephoneModel? telephoneModel = _fichaCadastroContextDB
+                                                    .TelephoneModels
+                                                    .Where(w => w.FichaId == Fichaid)
+                                                    .FirstOrDefault();
+
+
+                FichaTelephoneReadDTO fichaTelephoneReadDTO = _mapper.Map<FichaTelephoneReadDTO>(telephoneModel);
+
+                return StatusCode(HttpStatusCode.OK.GetHashCode(), fichaTelephoneReadDTO);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(HttpStatusCode.InternalServerError.GetHashCode(), ex);
+            }
+        }
+
     }
 }
--- FichaCadastroSln/FichaCadastroAPI/DTO/Telefone/TelephoneCreateDTO.cs
+++ FichaCadastroSln/FichaCadastroAPI/DTO/Telefone/TelephoneCreateDTO.cs
@@ -1,9 +1,9 @@
 ﻿public class TelephoneCreateDTO
 {
+    public int FichaId { get; set; }
     public string Ddd { get; set; }
     public string Number { get; set; }
     public bool ative { get; set; }
-
     public override string ToString() => $"({Ddd}) {Number}";
-   
+    
 }
